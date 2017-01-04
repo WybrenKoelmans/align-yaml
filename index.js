0a00b709e54a2b252fadcafcef4b4c8b9722c117
Wybren Koelmans <wybrenkoelmans@gmail.com>
2017-01-04 09:28:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const repeat = require('repeat-string');
 const getLongest = require('longest');
 
-module.exports = function(str, pad) {
-  var props = str.match(/^\s*[\S]+:/gm);
-  var longest = getLongest(props).length + (pad || 0);
+function getMatches(string, regex, index) {
+  index || (index = 1); // default to the first capturing group
+  var matches = [];
+  var match;
+  while (match = regex.exec(string)) {
+    matches.push(match[index]);
+  }
+  return matches;
+}
 
-  return str.split('\n').map(function(str) {
-    var line = /^(\s*.+[^:#]: )\s*(.*)/gm;
+module.exports = function (str, pad) {
+    var blocks = str.split('\n\n');
+    var ret = [];
+    for (block of blocks) {
+        var props = getMatches(block, /^(\s*[\S]+?:).*\S/gm, 1);
+        if (props.length === 0) {
+            ret.push(block);
+            continue;
+        }
 
-    return str.replace(line, function(match, $1, $2) {
-      var len = longest - $1.length + 1;
-      return $1 + repeat(' ', len) + $2;
-    });
-  }).join('\n');
+        var longest = getLongest(props).length + (pad || 0);
+        var item = block.split('\n').map(function (rawLine) {
+            var line = /^(\s*.+?[^:#]: )\s*(.*)/gm;
+
+            return rawLine.replace(line, function (match, $1, $2) {
+                var len = longest - $1.length + 1;
+                return $1 + repeat(' ', len) + $2;
+            });
+        }).join('\n');
+
+        // console.log(item);
+        ret.push(item);
+    }
+
+    return ret.join('\n');
 };
